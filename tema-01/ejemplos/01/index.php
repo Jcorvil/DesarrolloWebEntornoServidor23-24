73b341477189995aae3cea64ea83e393332ed6bc
unknown <jcorvil600@g.educaand.es>
2023-09-20 14:35:40 +0200

    20/09/2023. Commit 1. Se añaden los directorios y primer ejemplo de PHP

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        Ejemplo 1 -PHP
+        Hola Mundo
+    */
+
+    echo '<h1>';
+    echo '<center>';
+    echo 'Hola Mundo PHP';
+    echo'<BR>';
+    echo 'Soy Jorge Coronil Villalba';
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+
+?>
