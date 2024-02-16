fd177242988237acd9f5c020178116a5efdb26d3
unknown <jcorvil600@g.educaand.es>
2024-02-16 15:00:42 +0100

    16/02/2024. Comienzo de laravel

---
+++ tema-12/actividades/01/laravel_02/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Hola mundo Laravel 10';
+    // return view('welcome');
+});
+
+
+Route::get('/client', function () {
+    return 'Vista principal de clientes';
+});
+
+
+Route::get('/client/edit/{id}', function ($id) {
+    return "Cliente Editado: {$id}";
+});
+
+
+Route::get('/client/show/{id}', function ($id) {
+    return "Mostrando el cliente {$id}";
+});
+
+
+Route::get('/client/new/', function () {
+    return "Creando el cliente";
+});
+
+
+// Al poner '?' se convierte el parámetro en opcional. Se debe establecer que el parámetro opcional es null por defecto. 
+Route::get('/client/delete/{id1}/{id2?}', function ($id1, $id2 = null) {
+    if (!$id2)
+        return "Eliminar el cliente {$id1}";
+    else
+        return "Eliminar los clientes del {$id1} al {$id2}";
+});
